f3afea426ea29f70138c3af7cd002a2e6ad8d830
root <root@puppet.localdomain>
2019-09-04 02:49:08 +0100

    Update rspec test line
    This shows full tests results

--- test-puppet.sh
+++ test-puppet.sh
@@ -1,5 +1,6 @@
 # install the gems to a local subdirectory
 bundle install --path=.bundle/gems
 #Automated testing
-bundle exec rake spec
+#bundle exec rake spec
+SPEC_OPTS="--format documentation" bundle exec rake spec
 
